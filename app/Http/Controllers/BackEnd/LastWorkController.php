74a9facfb9cbf131470563e4e00e101a614ab520
hossam <hossamameen948@gmail.com>
2023-07-22 17:57:57 +0300

    add last work in dashboard

---
+++ app/Http/Controllers/BackEnd/LastWorkController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\BackEnd;
+use App\Models\LastWork;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+class LastWorkController extends BackEndController
+{
+    public function __construct(LastWork $model)
+    {
+        parent::__construct($model);
+    }
+    public function store(Request $request){
+       
+        $requestArray = $request->all();
+        if($request->hasFile('image'))
+        { 
+            $fileName = $this->uploadImage( $request , 530 , 432 );
+          if(isset($requestArray['image']) )
+          $requestArray['image'] =  $fileName;
+        }
+        $requestArray['user_id'] = Auth::user()->id;
+        $requestArray['type'] = join(",",$request->type);
+        $item = $this->model->create($requestArray);
+        if($request->item_order == null)
+        {
+            $item->item_order = $item->id + 2;
+            $item->save();
+        }
+        session()->flash('action', 'تم الاضافه بنجاح');
+        return redirect()->route($this->getClassNameFromModel().'.index');
+    }
+
+    public function update($id , Request $request){
+        $requestArray = $request->all();
+       
+        if($request->hasFile('image'))
+        {
+            $fileName = $this->uploadImage( $request ,530 , 432 );
+          if(isset($requestArray['image']) )
+          $requestArray['image'] =  $fileName;
+        }
+        $row = $this->model->FindOrFail($id);        
+        $requestArray['user_id'] = Auth::user()->id;
+        $requestArray['type'] = join(",",$request->type);
+        
+        $row->update($requestArray);
+        session()->flash('action', 'تم التحديث بنجاح');
+        return redirect()->route($this->getClassNameFromModel().'.index');
+    }
+}
